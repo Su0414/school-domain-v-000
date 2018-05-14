790892ea07246ac646ecde1d50a1d69518215a7a
Suchitra Naik <susanaik@gmail.com>
2018-05-14 21:31:29 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -29,11 +29,18 @@ class School
   end 
   
   def sort 
+<<<<<<< HEAD
     @newR={}
    @roster.map{|gr, name|
      @newR[gr] = name.sort
      }
    @newR
+=======
+   @newroster = {}
+   #puts @roster
+   @newroster.map{@roster.sort{|k,v| v <=> k}}
+   puts @newroster
+>>>>>>> 72c49eaed489d321356489270cb36a7498afd82b
   end 
   
   
